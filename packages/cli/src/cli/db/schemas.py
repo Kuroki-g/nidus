eca661a806c74ca6a56b7c53a37594af730d5ce5
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2026-02-25 08:21:35 +0900

    feat: add schemas to be implemented

--- packages/cli/src/cli/db/schemas.py
+++ packages/cli/src/cli/db/schemas.py
@@ -1,13 +1,98 @@
+from pathlib import Path
+
 import pyarrow as pa
+from pydantic_settings import BaseSettings
 
 
-def get_doc_schema(vector_size: int):
-    fields = [
-        pa.field("vector", pa.list_(pa.float32(), vector_size)),
-        pa.field("text", pa.string()),
+class SchemaNames(BaseSettings):
+    Meta: str = "doc_meta"
+    Doc: str = "doc"
+    DocFullText: str = "doc_full_text"
+
+
+def _doc_index_fields():
+    return [
         pa.field("source", pa.string()),
-        pa.field("chunk_id", pa.int64()),
+        pa.field("doc_name", pa.string()),
     ]
+
+
+def get_doc_meta_schema_fields():
+    fields = [
+        *_doc_index_fields(),
+        pa.field("created", pa.date32()),
+        pa.field("updated", pa.date32()),
+    ]
+
+    return fields
+
+
+def get_doc_meta_schema():
+    fields = get_doc_meta_schema_fields()
     schema = pa.schema(fields)
 
     return schema
+
+
+def get_doc_schema_fields(vector_size: int):
+    fields = [
+        *_doc_index_fields(),
+        pa.field("vector", pa.list_(pa.float32(), vector_size)),
+        pa.field("chunk_id", pa.int64()),
+        pa.field("created", pa.date32()),
+    ]
+
+    return fields
+
+
+def get_doc_schema(vector_size: int):
+    fields = get_doc_schema_fields(vector_size)
+    schema = pa.schema(fields)
+
+    return schema
+
+
+def get_doc_full_text_schema_fields():
+    fields = [
+        *_doc_index_fields(),
+        pa.field("full_text", pa.string()),
+    ]
+
+    return fields
+
+
+def get_doc_full_text_schema():
+    fields = get_doc_full_text_schema_fields()
+    schema = pa.schema(fields)
+
+    return schema
+
+
+def get_doc_text_schema_fields(vector_size: int):
+    fields = [
+        *_doc_index_fields(),
+        pa.field("vector", pa.list_(pa.float32(), vector_size)),
+        pa.field("chunk_id", pa.int64()),
+        pa.field("chunk_text", pa.string()),
+    ]
+
+    return fields
+
+
+def get_doc_text_schema(vector_size: int):
+    fields = get_doc_text_schema_fields(vector_size)
+    schema = pa.schema(fields)
+
+    return schema
+
+
+def get_file_hash(file_path: Path, algorithm="sha256"):
+    import hashlib
+
+    hash_func = hashlib.new(algorithm)
+
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_func.update(chunk)
+
+    return hash_func.hexdigest()
